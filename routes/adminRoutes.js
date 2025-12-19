db78f04df89aba5e41304d809b5cbcb49cdc445b
Olivia Gabriella Silitonga <oliviasilitonga03@gmail.com>
2025-12-19 18:26:09 +0700

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const verifyToken = require("../middleware/verifyToken");
+const authMiddleware = require("../middleware/authMiddleware");
 const isAdmin = require("../middleware/isAdmin");
 const adminController = require("../controllers/adminController");
 
 // Lagu
-router.post("/songs", verifyToken, isAdmin, adminController.addSong);
-router.put("/songs/:id", verifyToken, isAdmin, adminController.updateSong);
-router.delete("/songs/:id", verifyToken, isAdmin, adminController.deleteSong);
+router.post("/songs", authMiddleware, isAdmin, adminController.addSong);
+router.put("/songs/:id", authMiddleware, isAdmin, adminController.updateSong);
+router.delete("/songs/:id", authMiddleware, isAdmin, adminController.deleteSong);
 
 // User
-router.get("/users", verifyToken, isAdmin, adminController.getAllUsers);
+router.get("/users", authMiddleware, isAdmin, adminController.getAllUsers);
 
 module.exports = router;
