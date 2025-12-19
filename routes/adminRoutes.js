d8dfeeb7c83fa808c6eed9362165e9d2942b56ee
Olivia Gabriella Silitonga <oliviasilitonga03@gmail.com>
2025-12-19 19:06:19 +0700

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const router = express.Router();
+
 const authMiddleware = require("../middleware/authMiddleware");
 const isAdmin = require("../middleware/isAdmin");
-const adminController = require("../controllers/adminController");
 
-// Lagu
-router.post("/songs", authMiddleware, isAdmin, adminController.addSong);
-router.put("/songs/:id", authMiddleware, isAdmin, adminController.updateSong);
-router.delete("/songs/:id", authMiddleware, isAdmin, adminController.deleteSong);
-
-// User
-router.get("/users", (req, res, next) => {
-  console.log("🔥 ADMIN USERS ROUTE HIT");
-  next();
-}, authMiddleware, isAdmin, adminController.getAllUsers);
-
+router.get("/users", authMiddleware, isAdmin, (req, res) => {
+  res.json({ message: "ADMIN USERS OK" });
+});
 
 module.exports = router;
