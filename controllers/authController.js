6516c10eda8e5062550b1252cbb7a10a3d6f2321
Olivia Gabriella Silitonga <oliviasilitonga03@gmail.com>
2025-12-19 20:56:29 +0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const user = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { v4: uuidv4 } = require('uuid');
