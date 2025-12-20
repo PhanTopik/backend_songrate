e3ac7a54a3ae2de5d9d28700ddb8cd491e8d3cbd
Olivia Gabriella Silitonga <oliviasilitonga03@gmail.com>
2025-12-20 21:52:16 +0700

    commit

--- app.js
+++ app.js
@@ -15,12 +15,37 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: true, // Allow all origins
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://localhost:5174",
+        process.env.FRONTEND_URL || "https://localhost:5173",
+      ];
+
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(null, true); // Allow all for now
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+// Add headers middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/auth", authRoutes);
